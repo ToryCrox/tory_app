bbe8f02f12d56b94a629f6684beeec859fe19131
ToryCrox <exceedutow@gmail.com>
2023-03-02 00:36:26 +0800

    改造ThrottleInkWell

--- pkgs/common/lib/src/widgets/throttle_tap.dart
+++ pkgs/common/lib/src/widgets/throttle_tap.dart
@@ -7,11 +7,31 @@ typedef OnTapThrottle = FutureOr<void> Function();
 /// 防止重复点击
 class ThrottleInkWell extends StatefulWidget {
   final Widget? child;
+
   /// 点击任务未完成再次点击无效
   final OnTapThrottle? onTap;
+
   /// 屏蔽多长时间内的重复点击，默认500ms
   final int throttleTime;
+
+  /// 颜色
+  final Color? color;
+
+  /// 圆角
   final BorderRadius? borderRadius;
+
+  /// 形状
+  final ShapeBorder? shape;
+
+  /// 阴影深度
+  final double elevation;
+
+  /// 宽高设置
+  final double? width;
+  final double? height;
+
+  /// padding
+  final EdgeInsetsGeometry? padding;
 
   const ThrottleInkWell({
     Key? key,
@@ -19,6 +39,12 @@ class ThrottleInkWell extends StatefulWidget {
     this.onTap,
     this.throttleTime = 500,
     this.borderRadius,
+    this.color,
+    this.shape,
+    this.elevation = 0,
+    this.width,
+    this.height,
+    this.padding,
   }) : super(key: key);
 
   @override
@@ -31,12 +57,34 @@ class _ThrottleInkWellState extends State<ThrottleInkWell> {
 
   @override
   Widget build(BuildContext context) {
+    final isTransparent =
+        widget.color == null || widget.color == Colors.transparent;
+    Widget? child = widget.child;
+    if (widget.padding != null) {
+      child = Padding(
+        padding: widget.padding!,
+        child: child,
+      );
+    }
+    if (widget.width != null || widget.height != null) {
+      child = ConstrainedBox(
+        constraints: BoxConstraints.tightFor(
+          height: widget.height,
+          width: widget.width,
+        ),
+        child: child,
+      );
+    }
+
     return Material(
-      color: Colors.transparent,
+      type: isTransparent ? MaterialType.transparency : MaterialType.canvas,
+      color: widget.color ?? Colors.transparent,
+      shape: isTransparent ? null : widget.shape,
+      borderRadius: isTransparent ? null : widget.borderRadius,
+      elevation: widget.elevation,
       child: InkWell(
-        onTap: _isTaping || widget.onTap == null
-            ? null
-            : _doTap,
+        onTap: _isTaping || widget.onTap == null ? null : _doTap,
+        customBorder: widget.shape,
         borderRadius: widget.borderRadius,
         child: widget.child,
       ),
@@ -45,7 +93,7 @@ class _ThrottleInkWellState extends State<ThrottleInkWell> {
 
   Future _doTap() async {
     if ((DateTime.now().millisecondsSinceEpoch - _lastTapTime).abs() <
-        widget.throttleTime){
+        widget.throttleTime) {
       return;
     }
     setState(() {
@@ -61,12 +109,13 @@ class _ThrottleInkWellState extends State<ThrottleInkWell> {
   }
 }
 
-
 /// 防止重复点击
 class ThrottleTapDetector extends StatefulWidget {
   final Widget? child;
+
   /// 点击任务未完成再次点击无效
   final OnTapThrottle? onTap;
+
   /// 屏蔽多长时间内的重复点击，默认500ms
   final int throttleTime;
   final HitTestBehavior? behavior;
@@ -84,7 +133,6 @@ class ThrottleTapDetector extends StatefulWidget {
 }
 
 class _ThrottleTapDetectorState extends State<ThrottleTapDetector> {
-
   int _lastTapTime = 0;
   bool _isTaping = false;
 
@@ -98,7 +146,8 @@ class _ThrottleTapDetectorState extends State<ThrottleTapDetector> {
   }
 
   Future _doTap() async {
-    if (_isTaping || widget.onTap == null ||
+    if (_isTaping ||
+        widget.onTap == null ||
         (DateTime.now().millisecondsSinceEpoch - _lastTapTime).abs() <
             widget.throttleTime) {
       return;
